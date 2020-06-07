30d6d5e1a540a5689e3b3d7fc2099ce2521ae2f4
Weihan Li <weihanli@outlook.com>
2020-06-07 10:34:44 +0800

    Update csharp timestamp (#62)
    * fix merge conflict
    
    * update csharp sdk _getTimestamp

--- csharp/UnitTest/Payment/App/ClientTest.cs
+++ csharp/UnitTest/Payment/App/ClientTest.cs
@@ -17,13 +17,8 @@ namespace UnitTest.Payment.App
             Alipay.EasySDK.Payment.App.Models.AlipayTradeAppPayResponse response = Factory.Payment.App().Pay("iPhone6 16G",
                 "f4833085-0c46-4bb0-8e5f-622a02a4cffc", "0.10");
 
-<<<<<<< HEAD
             Assert.IsTrue(response.Body.Contains("app_id=2019022663440152&biz_content=%7b%22subject%22%3a%22iPhone6+16G%22%2c%22" +
                 "out_trade_no%22%3a%22f4833085-0c46-4bb0-8e5f-622a02a4cffc%22%2c%22total_amount%22%3a%220.10%22%7d&charset=UTF-8&" +
-=======
-            Assert.IsTrue(response.Body.Contains("app_id=2019022663440152&biz_content=%7b%22subject%22%3a%22iPhone6+16G%22%2c%22" +
-                "out_trade_no%22%3a%22f4833085-0c46-4bb0-8e5f-622a02a4cffc%22%2c%22total_amount%22%3a%220.10%22%7d&charset=UTF-8&" +
->>>>>>> dev
                 "format=json&method=alipay.trade.app.pay&notify_url=https%3a%2f%2fwww.test.com%2fcallback&sign="));
         }
     }
