e591aaea8bcff3c8b9f31f8ee7770e3a5f88fec1
Bean <xbol@live.com>
2020-11-28 13:48:16 +0800

    修复 PHP SDK 部分接口 asyncNotify 等方法无效问题

--- php/src/Kernel/EasySDKKernel.php
+++ php/src/Kernel/EasySDKKernel.php
@@ -400,16 +400,16 @@ class EasySDKKernel
 
     private function addOtherParams($textParams, $bizParams)
     {
-        if ($this->optionalTextParams == null) {
+        if ($this->optionalTextParams === null) {
             $this->textParams = $textParams;
         }
-        if ($textParams != null && $this->optionalTextParams != null) {
+        if ($textParams !== null && $this->optionalTextParams !== null) {
             $this->textParams = array_merge($textParams, $this->optionalTextParams);
         }
-        if ($this->optionalBizParams == null) {
+        if ($this->optionalBizParams === null) {
             $this->bizParams = $bizParams;
         }
-        if ($bizParams != null && $this->optionalBizParams != null) {
+        if ($bizParams !== null && $this->optionalBizParams !== null) {
             $this->bizParams = array_merge($bizParams, $this->optionalBizParams);
         }
     }
